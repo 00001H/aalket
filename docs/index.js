9aa5858cc00c4ee17079024b46b3dc5d6d3397d8
00001H <Programmer00001H@gmail.com>
2025-07-19 04:25:30 +0800

    first commit

---
+++ docs/index.js
@@ -0,0 +1,22 @@
+"use strict";
+const WDB = {
+    banjo: "(q.) (+n.) the count of, the number of",
+    meda: "(n.) [>] thing, something",
+    a: "(sm.) (positive statement)",
+    i: "(sm.) (adds context markers, aw., subjects and/or objects to the following sentence)",
+    ti: "(n.) [0] me",
+    miku: "(rw.) receive (information, knowledge)",
+    kipo: "(n.) [0] image, view, scenery",
+    asen: "(v.) (+n.) go within (something), enter (something)",
+    frio: "(n.) [+] building"
+};
+class WordRef extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.title = WDB[this.textContent];
+    }
+}
+;
+customElements.define("w-ref", WordRef);
